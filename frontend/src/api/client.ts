ec6c889b242db1d095b39e78faa96d771885f39d
Sharon Chen <sharon.chen@gatesfoundation.org>
2026-06-30 22:06:04 -0700

    change back for fallback baseURL to api

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // @ts-ignore
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
